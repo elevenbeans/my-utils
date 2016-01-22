0174beb98866d3a230003aed570fae9c76ed3cc1
lb102025 <lb102025@alibaba-inc.com>
2016-01-22 17:48:55 +0800

    add insertAfter

--- src/Utils.js
+++ src/Utils.js
@@ -21,10 +21,23 @@ var Utils = {
 		if (arr_url && arr_url[1]) {
 		   	var str_para = arr_url[1], result;
 			while ((result = reg_para.exec(str_para)) != null) {
+				console.log(result);
 				ret[result[1]] = result[2];
 			}
 		}
+		console.log(ret);
 		return ret;
+	},
+	insertAfter : function (draggedElement, targetElement){
+		var parent = targetElement.parentNode;
+		if (parent.lastChild == targetElement) {
+		// 如果最后的节点是目标元素，则直接添加。因为默认是最后
+			parent.appendChild(draggedElement);
+		}
+		else {
+			parent.insertBefore(draggedElement, targetElement.nextSibling);
+			//如果不是，则插入在目标元素的下一个兄弟节点 的前面。也就是目标元素的后面
+		}
 	}
 };
 
