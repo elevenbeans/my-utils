fea334e88cc85d40659c8d503eb10fa75b904a38
lb102025 <lb102025@alibaba-inc.com>
2016-01-26 11:27:33 +0800

    add bottomload immediatly

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,11 @@
+/**
+	** Usefull functions in work
+	** elevenBeans
+**/
+
 
 /**
-
+ usefull function on data processing
 **/
 
 function dataUtils (){
@@ -24,25 +29,25 @@ dataUtils.prototype = {
 	    return result;
 	},
 	// prase || param String
-    parseQueryString: function (url) {
-		var reg_url = /^[^\?]+\?([\w\W]+)$/,
-			reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
-			arr_url = reg_url.exec(url),
-			ret = {};
-		if (arr_url && arr_url[1]) {
-		   	var str_para = arr_url[1], result;
-			while ((result = reg_para.exec(str_para)) != null) {
-				console.log(result);
-				ret[result[1]] = result[2];
-			}
+  parseQueryString: function (url) {
+	var reg_url = /^[^\?]+\?([\w\W]+)$/,
+		reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
+		arr_url = reg_url.exec(url),
+		ret = {};
+	if (arr_url && arr_url[1]) {
+	   	var str_para = arr_url[1], result;
+		while ((result = reg_para.exec(str_para)) != null) {
+			console.log(result);
+			ret[result[1]] = result[2];
 		}
-		console.log(ret);
-		return ret;
+	}
+	console.log(ret);
+	return ret;
 	}
 };
 
 /**
-
+ usefull function on dom processing
 **/
 
 function domUtils (){
@@ -64,7 +69,7 @@ domUtils.prototype = {
 };
 
 /**
-
+	BottomLoader
 **/
 
 function BottomLoader(){
@@ -78,46 +83,44 @@ function BottomLoader(){
 }
 
 BottomLoader.prototype = {
-    init: function () { //对于detect检测做了间隔BUFF限制 并绑定scroll事件
-        var self = this;
+  init: function () { //对于detect检测做了间隔BUFF限制 并绑定scroll事件
+    var self = this;
 
-        var timer = null;
-        window.addEventListener('scroll', function () {
-            if (typeof timer === 'number') {
-                clearTimeout(timer);
-            }
-            timer = setTimeout(function () {
-                //添加onscroll事件处理
-                self.detect();
-            }, self.BUFF);
-        }, false);
-    },
-    detect: function () {  //监测的逻辑
-    	var self = this;
-        var docHeight = document.body.clientHeight;
-        var scrollTop = document.body.scrollTop; //scroll distance
-        console.log('detect');
-        var elBottomPos = docHeight;
-
-        console.log(self.viewportHeight,scrollTop,cbContent.diff,elBottomPos)
-
-        if ((self.viewportHeight + scrollTop + cbContent.diff >= elBottomPos )&&(scrollTop + cbContent.diff <= elBottomPos )){
-            cbContent.callback && cbContent.callback(self.count);
-            console.log('Loader '+self.count+" times");
-            self.count ++;
+    var timer = null;
+    window.addEventListener('scroll', function () {
+        if (typeof timer === 'number') {
+            clearTimeout(timer);
         }
-    },
-    addCallback: function (callback,config) {  //增加回调函数
-        var self = this;
-        cbContent = {
-            //id: S.guid(),
-            diff: config.diff || self.DIFF,
-            callback: callback,
-            el: config.el
-        };
-        if(config.immediately){
+        timer = setTimeout(function () {
+            //添加onscroll事件处理
             self.detect();
-        }
+        }, self.BUFF);
+    }, false);
+  },
+  detect: function () {  //监测的逻辑
+  	var self = this;
+    var docHeight = document.body.clientHeight;
+    var scrollTop = document.body.scrollTop; //scroll distance
+    console.log('detect');
+    var elBottomPos = docHeight;
+
+    console.log(self.viewportHeight,scrollTop,cbContent.diff,elBottomPos)
+
+    if ((self.viewportHeight + scrollTop + cbContent.diff >= elBottomPos )&&(scrollTop + cbContent.diff <= elBottomPos )){
+      cbContent.callback && cbContent.callback(self.count);
+      console.log('Loader '+self.count+" times");
+      self.count ++;
     }
+  },
+  addCallback: function (callback,config) {  //增加回调函数
+    var self = this;
+    cbContent = {
+      diff: config.diff || self.DIFF,
+      callback: callback
+    };
+    if(config.immediately){
+      self.detect();
+    }
+  }
 };
 
