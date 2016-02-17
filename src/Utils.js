5ec508c1baafb4317650ff5fbee1441a49ee3ce0
lb102025 <lb102025@alibaba-inc.com>
2016-02-17 16:17:24 +0800

    delete old parseQueryString

--- src/Utils.js
+++ src/Utils.js
@@ -28,23 +28,7 @@ dataUtils.prototype = {
     }
     return result;
 	},
-	// prase || param String
- //  parseQueryString: function (url) {
-	// 	var reg_url = /^[^\?]+\?([\w\W]+)$/,
-	// 		reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
-	// 		arr_url = reg_url.exec(url),
-	// 		ret = {};
-	// 	if (arr_url && arr_url[1]) {
-	// 	   	var str_para = arr_url[1], result;
-	// 		while ((result = reg_para.exec(str_para)) != null) {
-	// 			console.log(result);
-	// 			ret[result[1]] = result[2];
-	// 		}
-	// 	}
-	// 	console.log(ret);
-	// 	return ret;
-	// },
-   parseQueryString: function(url) {  
+  parseQueryString: function(url) {  
     var result = {};
     var paramParts;
     var params = (url.split('?')[1] || '').split('&');  
