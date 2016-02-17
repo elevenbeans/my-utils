adefbb4ae45764d870e3cf43e4ae3ac52d7367fc
lb102025 <lb102025@alibaba-inc.com>
2016-02-17 21:14:35 +0800

    add throttle mode for bottomloader

--- src/Utils.js
+++ src/Utils.js
@@ -69,39 +69,42 @@ domUtils.prototype = {
 	BottomLoader
 **/
 
-function BottomLoader(){
-	this.BUFF = 400;
+function BottomLoader(isDebounce){
+	this.BUFF = 200;
 	this.viewportHeight = window.screen.height;
 	this.DIFF = this.viewportHeight;
 	this.cbContent = {};
 	this.count = 1;
-	
-	this.init();
+	this.last = 0;
+	this.init(isDebounce);
 }
 
 BottomLoader.prototype = {
-  init: function () { //对于detect检测做了间隔BUFF限制 并绑定scroll事件
+  init: function (isDeb) { //对于detect检测做了间隔BUFF限制 并绑定scroll事件
     var self = this;
-
     var timer = null;
     window.addEventListener('scroll', function () {
-      if (typeof timer === 'number') {
-          clearTimeout(timer);
+      if(isDeb){
+        if (typeof timer === 'number') {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function () {
+            //添加onscroll事件处理
+            self.detect();
+        }, self.BUFF);
+      }else{
+        self.throttle(400,self.detect);
       }
-      timer = setTimeout(function () {
-          //添加onscroll事件处理
-          self.detect();
-      }, self.BUFF);
     }, false);
   },
   detect: function () {  //监测的逻辑
   	var self = this;
     var docHeight = document.body.clientHeight;
     var scrollTop = document.body.scrollTop; //scroll distance
-    console.log('detect');
+    //console.log('detect');
     var elBottomPos = docHeight;
 
-    console.log(self.viewportHeight,scrollTop,cbContent.diff,elBottomPos)
+    //console.log(self.viewportHeight,scrollTop,cbContent.diff,elBottomPos)
 
     if ((self.viewportHeight + scrollTop + cbContent.diff >= elBottomPos )&&(scrollTop + cbContent.diff <= elBottomPos )){
       cbContent.callback && cbContent.callback(self.count);
@@ -118,6 +121,15 @@ BottomLoader.prototype = {
     if(config.immediately){
       self.detect();
     }
+  },
+  throttle : function(delay, action){
+    var self = this;
+    var curr = +new Date();
+    if (curr - self.last > delay){
+      console.log('in');
+      action.apply(this, arguments);
+      self.last = curr ;
+    };
   }
 };
 
