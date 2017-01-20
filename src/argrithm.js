c7f2e19a79befa8c1e03c049109280f8ea87a0dd
elevenBeans <elevenbeansf2e@gmail.com>
2017-01-20 18:41:40 +0800

    add insertSort

--- src/argrithm.js
+++ src/argrithm.js
@@ -1,13 +1,10 @@
 ;(function(){
 
 	var argrithm = {
-		bubbleSort: function(arr) {
-			//var res = [];
-			for(var i = 0; i < arr.length - 1; i++) {
-				//if(i = 0) res.push(arr[0]);
-				for(var j = 1; j < arr.length - 2; j++) {
-					if(arr[j-1] > arr[j]) {
-						// var temp;
+		bubbleSort: function(arr){
+			for(var i = 0; i < arr.length - 1; i++){
+				for(var j = 1; j < arr.length - 2; j++){
+					if(arr[j-1] > arr[j]){
 						temp = arr[j];
 						arr[j] = arr[j-1];
 						arr[j-1] = temp;
@@ -16,30 +13,36 @@
 			}
 			return arr;
 		},
-		quickSort: function(arr) {
-        //如果数组<=1,则直接返回
-        if(arr.length<=1){return arr;}
-        var pivotIndex=Math.floor(arr.length/2);
-        //找基准，并把基准从原数组删除
-        var pivot=arr.splice(pivotIndex,1)[0];
-        //定义左右数组
-        var left=[];
-        var right=[];
+		quickSort: function(arr){
+			if(arr.length < 2) return arr;
+			var middleIndex = Math.floor(arr.length / 2);
+			var middleValue = arr.splice(middleIndex,1)[0];
+			
+			var left = [];
+			var right = [];
 
-        //比基准小的放在left，比基准大的放在right
-        for(var i=0;i<arr.length;i++){
-            if(arr[i]<=pivot){
-                left.push(arr[i]);
-            }
-            else{
-                right.push(arr[i]);
-            }
-        }
-        //递归
-        return this.quickSort(left).concat([pivot],this.quickSort(right));
+			for(var i = 0; i < arr.length; i++){
+				if(arr[i] < middleValue){
+					left.push(arr[i]);
+				}else{
+					right.push(arr[i]);
+				}
+			}
+			return this.quickSort(left).concat(middleValue,this.quickSort(right));
     },
-		insertionSort: function(){
-
+		insertionSort(arr) {
+	    var len = arr.length;
+	    var preIndex, current;
+	    for (var i = 1; i < len; i++) {
+	        preIndex = i - 1;
+	        current = arr[i];
+	        while(preIndex >= 0 && arr[preIndex] > current) {
+	            arr[preIndex+1] = arr[preIndex];
+	            preIndex--;
+	        }
+	        arr[preIndex+1] = current;
+	    }
+	    return arr;
 		},
 		shellSort: function(){
 
