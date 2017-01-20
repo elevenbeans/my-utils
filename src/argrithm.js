df9e25b01d4725c74a2dfa1cc2dd0effea625fab
elevenBeans <elevenbeansf2e@gmail.com>
2017-01-20 15:33:15 +0800

    add quickSort

--- src/argrithm.js
+++ src/argrithm.js
@@ -16,9 +16,28 @@
 			}
 			return arr;
 		},
-		quickSort: function(){
+		quickSort: function(arr) {
+        //如果数组<=1,则直接返回
+        if(arr.length<=1){return arr;}
+        var pivotIndex=Math.floor(arr.length/2);
+        //找基准，并把基准从原数组删除
+        var pivot=arr.splice(pivotIndex,1)[0];
+        //定义左右数组
+        var left=[];
+        var right=[];
 
-		},
+        //比基准小的放在left，比基准大的放在right
+        for(var i=0;i<arr.length;i++){
+            if(arr[i]<=pivot){
+                left.push(arr[i]);
+            }
+            else{
+                right.push(arr[i]);
+            }
+        }
+        //递归
+        return this.quickSort(left).concat([pivot],this.quickSort(right));
+    },
 		insertionSort: function(){
 
 		},
@@ -30,6 +49,6 @@
 		}
 	}
 
-	window.argrithm = argrithm;
+	window.Argrithm = argrithm;
 
 })(window);
