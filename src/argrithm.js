11e55f2c76614fafb629ddc21ec112861855adc5
elevenBeans <elevenbeansf2e@gmail.com>
2017-01-22 13:47:07 +0800

    add shellSort

--- src/argrithm.js
+++ src/argrithm.js
@@ -36,16 +36,26 @@
 	    for (var i = 1; i < len; i++) {
 	        preIndex = i - 1;
 	        current = arr[i];
-	        while(preIndex >= 0 && arr[preIndex] > current) {
-	            arr[preIndex+1] = arr[preIndex];
+	        while(preIndex >= 0 && arr[preIndex] > current) { // 找位置
+	            arr[preIndex + 1] = arr[preIndex];
 	            preIndex--;
 	        }
-	        arr[preIndex+1] = current;
+	        arr[preIndex + 1] = current; // 插入
 	    }
 	    return arr;
 		},
-		shellSort: function(){
-
+		shellSort: function(arr) {
+			var len = arr.length;
+	    for (var fraction = Math.floor(len / 2); fraction > 0; fraction = Math.floor(fraction / 2)) {
+	      for (var i = fraction; i < len; i++) {
+	        for (var j = i - fraction; j >= 0 && arr[j] > arr[fraction + j]; j -= fraction) {
+	          var temp = arr[j];	            
+	          arr[j] = arr[fraction + j];
+            arr[fraction + j] = temp;
+	        }
+	      }
+	    }
+		  return arr;
 		},
 		binarySearch: function(){
 
