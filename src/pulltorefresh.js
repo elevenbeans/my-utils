93a553bebd5ec3ab3f4ce72e7173c2a6bb4b760c
elevenBeans <elevenbeansf2e@gmail.com>
2017-09-17 04:00:40 +0800

    done

--- src/pulltorefresh.js
+++ src/pulltorefresh.js
@@ -9,27 +9,26 @@
 			_element = document.querySelector(cfg.targetElement || 'body');
 			_ptrEle = document.querySelector(cfg.ptrElement);
 			_ptrEle.innerText = cfg.instructionsPullToRefresh;
-			document.addEventListener('touchstart', function(event){
+			_element.addEventListener('touchstart', function(event){
 				_startLength = event.touches[0].pageY;
 				_element.removeAttribute('style');
-				// pull to refresh
+				// 'pull to refresh'
 				_ptrEle.innerText = cfg.instructionsPullToRefresh;
 			});
-			document.addEventListener('touchmove', function(event){
-				if(document.body.scrollTop === 0){
+			_element.addEventListener('touchmove', function(event){
+				if(_element.scrollTop === 0){
 					_pullLengh = event.touches[0].pageY - _startLength;
-					// pull to refresh
+					// 'pull to refresh'
 					pullElement(_element, _pullLengh, cfg);
 				}
-				if(document.body.scrollTop > 0){
-					// window.scrollTo(0, 0);
+				if(_element.scrollTop > 0){
 					_element.style['transform'] = 'translate(0, 0px)';
 				}
 			});
-			document.addEventListener('touchend', function(event){
-				if(document.body.scrollTop === 0){
+			_element.addEventListener('touchend', function(event){
+				if(_element.scrollTop === 0){
 					if(_pullLengh > 60 ){
-						// refreshing
+						// 'refreshing'
 						_ptrEle.innerText = cfg.instructionsRefreshing;
 						cfg.onPull();
 						_element.style['transition'] = 'transform 0.4s ease';
@@ -46,13 +45,10 @@
 		if(length < 100){
 			element.style['transform'] = 'translate(0, ' + length + 'px)';
 			if(length > 60){
-				// release to fresh
+				// 'release to fresh'
 				_ptrEle.innerText = cfg.instructionsReleaseToRefresh;
 			}
 		}
 	};
-	// let setPullInstractions = function(ptrEle, text){
-	// 	ptrEle.innerText = text;
-	// };
 	window.pullToRefresh = pullToRefresh;
 })(window);
